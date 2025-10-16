76b72cace73cb0209526a07205f0627648426b78
Kelly MacDevette <kellymacdev@gmail.com>
2025-10-16 12:56:08 +0200

    fixed viewport things

--- project4/settings.py
+++ project4/settings.py
@@ -91,6 +91,10 @@ DATABASES = {
 #    }
 #}
 
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+
 AUTH_USER_MODEL = "network.User"
 
 # Password validation
@@ -129,7 +133,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
